c3b7a22cf6c0d4a5b6b6ff7aebd4d2080c7ecea7
sincollmm <81744519+sincollmm@users.noreply.github.com>
2022-07-28 09:39:02 +0700

    update port

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,9 +1,8 @@
 var config = {
-    user: 'vido01',
-    password: 'Vido@01',
-    server: 'zalovido.pmsa.com.vn',
-    port:4020,
-    database: 'Vido',
+    user: 'sincollmm',
+    password: 'zxcZXCV123',
+    server: 'localhost',
+    database: 'QLDT',
     trustServerCertificate: true
 };
 

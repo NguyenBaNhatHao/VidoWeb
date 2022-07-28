66f4715e3bca0426f74e6edbb7205ecc40433b65
sincollmm <81744519+sincollmm@users.noreply.github.com>
2022-07-28 10:07:36 +0700

    back to zaloserver

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,6 +3,7 @@ var config = {
     user: process.env.USERDB,
     password: process.env.PASSWORD,
     server: process.env.HOST,
+    port: parseInt(process.env.PORTDB, 10),
     database: process.env.DATABASE,
     trustServerCertificate: true
 };
